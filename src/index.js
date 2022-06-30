c7ebf53f331f8a6bc6ebefb35e321f0c69f6a76a
Alexaoj <alexandra.ojeda@instaleap.io>
2022-06-30 03:11:30 -0500

    Index

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = required ('express');
+const morgan = require ('morgan');
+
+// initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000 );
+
+// Middlewares
+
+app.use(morgan('dev'));
+
+// Global Variables 
+
+// Routes
+
+//public
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
